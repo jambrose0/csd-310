17ea1db606496be33bd427cdddf41b65d93e873b
Jacob Ambrose <jambrose0518@gmail.com>
2024-02-21 21:24:46 -0500

    initial python scripting setup.

---
+++ module-8/willson_financial_initial.py
@@ -0,0 +1,44 @@
+# Jacob Ambrose 02/17/2024
+# Module 8.2
+
+# imports mysql connector
+import mysql.connector
+from mysql.connector import errorcode
+
+
+
+# defines user login information
+config = {
+    "user": "movies_user",
+    "password": "popcorn",
+    "host": "127.0.0.1",
+    "database": "movies",
+    "raise_on_warnings": True
+}
+
+# connects to database
+try:
+    db  = mysql.connector.connect(**config)
+    
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))   
+    
+    input("\n\n Press any key to continue... ")
+    print('\n \n \n')
+    
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print(" The supplied username or password are invalid")
+        
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print(" The specified database does not exist")
+        
+    else:
+        print(err)
+        
+finally:
+    db.close()
+    
+    
+# connect db to cursor
+db = mysql.connector.connect(**config)
+cursor = db.cursor()  
